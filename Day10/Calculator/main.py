ebe4c5e3ca43b3d81c4639d632101e8d1d8da7e6
JJ <Joshua Jenkins>
2022-01-28 14:07:24 -0700

    Day 10; Move Functions to own file

--- Day10/Calculator/main.py
+++ Day10/Calculator/main.py
@@ -1,63 +1,12 @@
 # Calculator
 from art import logo
-
-# Add
-def add(n1, n2):
-  """Add two number to each other
-
-  Args:
-  n1 (int): Addend 1.
-  n2 (int): Addend 2.
-
-  Returns:
-  int: The sum of the two addends.
-  """
-  return n1 + n2
-
-# Subtract
-def subtract(n1, n2):
-  """Subtract two numbers from each other
-
-  Args:
-  n1 (int): The number (minuend) that will be subtracted from
-  n2 (int): The amount (subtrahend) by which you subtract from another number
-
-  Returns:
-  int: The number (difference) that results from subtraction
-  """
-  return n1 - n2
-
-# Multiply
-def multiply(n1, n2):
-  """Multiply two numbers
-
-  Args:
-  n1 (int): Factor 1. 
-  n2 (int): Factor 2.
-
-  Returns:
-  int: The product of factor 1 and factor 2.
-  """
-  return n1 * n2
-
-# Divide
-def divide(n1, n2):
-  """Divide two numbers
-
-  Args:
-  n1 (int): The number that is being divided (dividend)
-  n2 (int): The number that is being divided by (divisor)
-
-  Returns:
-  int: The Quotient
-  """
-  return n1 / n2
+from functions import add, subtract, multiply, divide
 
 operations = {
 "+": add,
 "-": subtract,
 "*": multiply,
-"/": divide
+"/": divide,
 }
 
 def calculator():
@@ -67,13 +16,9 @@ def calculator():
   for key in operations:
     options += key + ' '
 
-  # function = operations["*"]
-  # function(2, 3)
-
   num1 = float(input("What's the first number?: "))
 
   continue_calculating = True
-
   while continue_calculating:
     chosen_operation = input(f"{options}\nPick an operation from the line above operation: ")
     num2 = float(input("What's the next number?: "))
@@ -86,6 +31,7 @@ def calculator():
       num1 = answer
     else:
       continue_calculating = False
+      ## RECURSION
       calculator()
 
 calculator()
