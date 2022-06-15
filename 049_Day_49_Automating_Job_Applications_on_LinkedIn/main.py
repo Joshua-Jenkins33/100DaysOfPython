ff07da12e32d4369f4d20f271abb309b3c34e723
Joshua Jenkins <joshuajenkins33@gmail.com>
2022-06-14 22:08:06 -0600

    Day 49; LinkedIn Job Search complete

--- 049_Day_49_Automating_Job_Applications_on_LinkedIn/main.py
+++ 049_Day_49_Automating_Job_Applications_on_LinkedIn/main.py
@@ -5,12 +5,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import time
 from dotenv import load_dotenv
 import os
 
-# print(os.path.relpath('G:\Main\Development\100DaysOfPython\chromedriver.exe', 'G:\Main\Development\100DaysOfPython\049_Day_49_Automating_Job_Applications_on_LinkedIn\main.py'))
-#python -c "import sys; print('\n'.join(sys.path))"
 
 
 # ====================================WEBDRIVER VARIABLES==================================== #
@@ -29,8 +28,8 @@ driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3075033946&f_AL=t
 
 
 load_dotenv(r'G:\Main\Development\100DaysOfPython\049_Day_49_Automating_Job_Applications_on_LinkedIn\.env')
-linkedin_username=os.getenv("USERNAME")
-linkedin_password=os.getenv("PASSWORD")
+linkedin_username=os.getenv("LINKEDINUSERNAME")
+linkedin_password=os.getenv("LINKEDINPASSWORD")
 
 
 # ====================================MAIN LOGIC==================================== #
@@ -48,3 +47,28 @@ username.send_keys(linkedin_username)
 password = driver.find_element(By.NAME, "session_password")
 password.send_keys(linkedin_password, Keys.ENTER)
 
+time.sleep(3)
+listings = driver.find_elements(By.CLASS_NAME, "jobs-search-results__list-item")
+
+for listing in listings:
+    listing.click()
+    time.sleep(2)
+    save_button = driver.find_element(By.CSS_SELECTOR, "button.jobs-save-button")
+    save_button.click()
+    print(f"{save_button.text} Button Clicked!")
+
+
+    # scroll to the bottom of the right rail
+    right_rail = driver.find_element(By.CLASS_NAME, "jobs-search__right-rail")
+    right_rail.click()
+    html = driver.find_element(By.TAG_NAME, "html")
+    html.send_keys(Keys.END)
+    time.sleep(1)
+
+    # follow button
+    try:
+        follow_button = driver.find_element(By.CSS_SELECTOR, 'button.follow')
+        follow_button.click()
+        print(f"{follow_button.text} Button Clicked!")
+    except NoSuchElementException:
+        continue
