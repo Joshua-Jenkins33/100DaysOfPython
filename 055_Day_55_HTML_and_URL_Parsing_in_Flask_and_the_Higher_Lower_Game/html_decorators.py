32a1b2392b241dc1d7f39f6632ccef39f79176fd
Joshua Jenkins <joshuajenkins33@gmail.com>
2022-06-26 11:07:25 -0600

    Day 55; Making flask html decorators

--- 055_Day_55_HTML_and_URL_Parsing_in_Flask_and_the_Higher_Lower_Game/html_decorators.py
+++ 055_Day_55_HTML_and_URL_Parsing_in_Flask_and_the_Higher_Lower_Game/html_decorators.py
@@ -1,32 +1,19 @@
-# import time
-# current_time = time.time()
-# print(current_time)
-
-# def speed_calc_decorator(function):
-#     def time_to_run():
-#         start_time = time.time()
-#         function()
-#         end_time = time.time()
-#         print(f"{function.__name__} took {end_time - start_time} seconds to run.")
-#     return time_to_run
-
-
-# @speed_calc_decorator
-# def fast_function():
-#     for i in range(10000000):
-#         i * i
-        
-        
-# @speed_calc_decorator
-# def slow_function():
-#     for i in range(100000000):
-#         i * i
-
-# fast_function()
-# slow_function()
-
 def make_bold(function):
     def add_b_tags():
         bold_html = f"<b>{function()}</b>"
         return bold_html
     return add_b_tags
+
+
+def make_emphasis(function):
+    def add_em_tags():
+        emphasis_html = f"<em>{function()}</em>"
+        return emphasis_html
+    return add_em_tags
+
+
+def make_underlined(function):
+    def add_u_tags():
+        underline_html = f"<u>{function()}</u>"
+        return underline_html
+    return add_u_tags
