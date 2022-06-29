c5462a92bb1fbe2562a0473a2a5c5a5e02805951
JJ <joshuajenkins33@gmail.com>
2022-06-28 22:30:57 -0600

    Day 57; Blog Card article is complete

--- 057_Day_57_Templating_with_Jinja_in_Flask_Applications/blog-templating-start/main.py
+++ 057_Day_57_Templating_with_Jinja_in_Flask_Applications/blog-templating-start/main.py
@@ -5,12 +5,23 @@ from post import Post
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+def get_articles():
     blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
     response = requests.get(blog_url)
     all_articles = [Post(blog_post) for blog_post in response.json()]
-    return render_template("index.html", articles=all_articles)
+    return all_articles
+
+@app.route('/')
+def home():
+    
+    return render_template("index.html", articles=get_articles())
+
+@app.route('/post/<int:blog_id>')
+def full_article(blog_id):
+    for article in get_articles():
+        if article.id == blog_id:
+            return render_template("post.html", article=article)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
